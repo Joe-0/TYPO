6b47df92dc1c3e16f1206d36d3a710fd607b67ca
LeVio <Rahul.vudaru@gmail.com>
2021-10-26 16:12:22 -0500

    moved timer js to typo.js

--- templates/typo.js
+++ templates/typo.js
@@ -1,6 +1,33 @@
 var challengeText = getElementbyID(textDisplay.innerText);
 var userText = getElementbyID(textInput.innerText);
 
+
+//timer stuff by joe
+const textDisplayElement = document.getElementById('textDisplay')
+const testInputElement = document.getElementById('textInput')
+const timeElapsedElement = document.getElementById('timeElapsed')
+
+testInputElement.addEventListener('input',timePassed)
+
+let startTime
+function timePassed() {
+    timeElapsedElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timeElapsedElement.innerText = getTimerTime()
+    }, 1000)
+    testInputElement.removeEventListener('input',timePassed)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}
+
+
+
+
+
+
 //listens for inputs
 typoInputElement.addEventListener('input', () => {
     const arrayChallenge = challengeText.querySelectorAll('span')
