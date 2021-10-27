6fd35cd3dfebbfb7ed6eefcfbfb3293fa598a50b
Joe-0 <sonofahamster@yahoo.com>
2021-10-27 18:08:35 -0500

    created new unit test file because the last file was causing issues

---
+++ flaskr_tests.py
@@ -0,0 +1,33 @@
+"""
+https://sun.iwu.edu/~mliffito/flask_tutorial/testing.html
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
+
+    :copyright: (c) 2015 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+import os
+import app as flaskr
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.testing = True
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_show_index(self):
+        rv = self.app.get('/')
+        assert b'Begin typing when ready' in rv.data
+
