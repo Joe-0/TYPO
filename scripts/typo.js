67bdefa27273726240826b57abc26c25dd9d33e1
LeVio <Rahul.vudaru@gmail.com>
2021-10-29 15:03:18 -0500

    just committing to pull

--- scripts/typo.js
+++ scripts/typo.js
@@ -1,14 +1,15 @@
 //Timer code adapted from https://www.youtube.com/watch?v=R-7eQIHRszQ
-var challengeText = getElementbyID(textDisplay.innerText);
-var userText = getElementbyID(textInput.innerText);
+
 
 
 //timer stuff by joe
-const textDisplayElement = document.getElementById('textDisplay')
+/*const textDisplayElement = document.getElementById('textDisplay')
 const testInputElement = document.getElementById('textInput')
 const timeElapsedElement = document.getElementById('timeElapsed')
 
 testInputElement.addEventListener('input',timePassed)
+
+
 
 let startTime
 function timePassed() {
@@ -17,18 +18,27 @@ function timePassed() {
     setInterval(() => {
         timeElapsedElement.innerText = getTimerTime()
     }, 1000)
+    getEndTime()
     testInputElement.removeEventListener('input',timePassed)
 }
 
 function getTimerTime() {
     return Math.floor((new Date() - startTime) / 1000)
 }
+let endTime
+
+function getEndTime(){
+    endTime = Math.floor((new Date() - startTime) / 1000)
+    console.log(getTimerTime())
+}
+
+*/
 
 
 
 
-
-
+var challengeText = getElementbyID(textDisplay.innerText);
+var userText = getElementbyID(textInput.innerText);
 //listens for inputs
 typoInputElement.addEventListener('input', () => {
     const arrayChallenge = challengeText.querySelectorAll('span')
