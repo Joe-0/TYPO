297b7e80066caf988c854edcb4eb9c3769097377
LeVio <Rahul.vudaru@gmail.com>
2021-10-30 18:07:02 -0500

    schema for aggregate wpms and accuracy add

--- schema.sql
+++ schema.sql
@@ -5,8 +5,17 @@ create table challengeText (
   'text' text not null
 );
 
+drop table if exists users;
 create table users (
-    id primary key autoincrement,
+    id integer primary key autoincrement,
     username text not null unique,
     password text not null unique 
+);
+
+drop table if exists attempts;
+create table attempts (
+    id integer primary key autoincrement,
+    user text not null references users(username),
+    wpm integer not null,
+    accuracy integer not null
 );
