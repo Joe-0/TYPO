5fd9f557afcfbe73e7b3919bc20bbdb6835ac8e1
flyiingfrogs <36997463+flyiingfrogs@users.noreply.github.com>
2021-11-02 11:50:42 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,21 @@
 drop table if exists challengeText;
-drop table if exists users;
 create table challengeText (
   id integer primary key autoincrement,
   title text not null,
   'text' text not null
 );
 
-
+drop table if exists users;
 create table users (
     id integer primary key autoincrement,
     username text not null unique,
-    password text not null unique 
+    password text not null unique,
+    isAdmin boolean not null
 );
+
+drop table if exists attempts;
+create table attempts {
+  id integer primary key autoincrement,
+  user text,
+  time not null
+}
