64ff1149d54ec255b37002a6247af420202e1011
alikhatai <67115491+alikhatai@users.noreply.github.com>
2021-11-28 15:51:34 -0600

    Update typo_tests.py
    got rid of login and logout functions for unit tests. will be making new ones later.

--- typo_tests.py
+++ typo_tests.py
@@ -14,17 +14,6 @@ import app as flaskr
 import unittest
 import tempfile
 
-
-def login(client, username, password):
-    return client.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
-
-
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
-
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
